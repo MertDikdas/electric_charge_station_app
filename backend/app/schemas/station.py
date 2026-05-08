684cd04dd18cc4102f357e0044898bedef92064a
0nryvz <onur.yavz.16@gmail.com>
2026-05-08 05:45:35 +0300

    fix(station): use company id in station schemas

--- backend/app/schemas/station.py
+++ backend/app/schemas/station.py
@@ -13,9 +13,9 @@ StationStatus = Literal[
 
 class StationCreate(BaseModel):
     address: str
-    company: str
     latitude: float = Field(..., ge=-90, le=90)
     longitude: float = Field(..., ge=-180, le=180)
+    company_id: int
     status: str = "AVAILABLE"
 
 class Station(StationCreate):
