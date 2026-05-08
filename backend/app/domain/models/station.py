d677aa39326abadc8d1fa549e59343de594430cb
0nryvz <onur.yavz.16@gmail.com>
2026-05-08 05:43:23 +0300

    fix(station): replace company field with company id

--- backend/app/domain/models/station.py
+++ backend/app/domain/models/station.py
@@ -2,15 +2,15 @@ from dataclasses import dataclass
 
 from app.domain.models.base import BaseEntity
 from app.domain.models.charger import ChargerEntity
-from typing import List, Optional, Dict, Any
+from typing import List
 from dataclasses import field
 
 @dataclass
 class StationEntity(BaseEntity):
     address: str
-    company: str
     latitude: float
     longitude: float
+    company_id: int
     status: str = "AVAILABLE"
     chargers: List[ChargerEntity] = field(default_factory=list)
 
